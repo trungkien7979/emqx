914dffdf1c01e9d27ecebf2debbf9fda45ee1e31
KienBanana <boom16vn@gmail.com>
2026-03-19 11:54:02 +0700

    update src

--- mqtt-server/dist/mqtt/mqtt.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { WsGateway } from '../ws/ws.gateway';
-export declare class MqttService implements OnModuleInit {
-    private readonly ws;
-    private client;
-    constructor(ws: WsGateway);
-    onModuleInit(): void;
-}
--- mqtt-server/dist/ws/ws.gateway.d.ts
+++
@@ -1,6 +0,0 @@
-import { Server } from 'socket.io';
-export declare class WsGateway {
-    server: Server;
-    handleConnection(client: any): void;
-    send(data: any): void;
-}
